a704ec57bf8813cb762adfbd81c6032278491cfd
lovesnow <houshao55@gmail.com>
2012-12-23 23:28:01 +0800

    update mprisv2.

--- plugins/mpris2/__init__.py
+++ plugins/mpris2/__init__.py
@@ -1,31 +1,24 @@
-# vim: ts=4:sw=4:et:
-#
-# Copyright (C) 2010 Sun Ning <classicning@gmail.com>
-#
-# This program is free software; you can redistribute it and/or modify
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2012 Deepin, Inc.
+#               2011 ~ 2012 Hou Shaohui
+# 
+# Author:     Hou Shaohui <houshao55@gmail.com>
+# Maintainer: Hou Shaohui <houshao55@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
+# the Free Software Foundation, either version 3 of the License, or
 # any later version.
-#
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
-#
-# The developers of the Exaile media player hereby grant permission
-# for non-GPL compatible GStreamer and Exaile plugins to be used and
-# distributed together with GStreamer and Exaile. This permission is
-# above and beyond the permissions granted by the GPL license by which
-# Exaile is covered. If you modify this code, you may extend this
-# exception to your version of the code, but you are not obligated to
-# do so. If you do not wish to do so, delete this exception statement
-# from your version.
-#
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 try:
     import indicate
