5855a4e84ef5b3294fe7a1e77d13c47a23c7ce0a
dragondjf <dragondjf@gmail.com>
2015-03-09 19:22:53 +0800

    fix

--- test/mutagenTest.py
+++ test/mutagenTest.py
@@ -10,7 +10,7 @@ from mutagen.mp3 import MP3
 audio = MP3(f)
 print audio.mime
 print audio.info.__dict__
-# print audio.size
+print audio.tags
 # print EasyID3.valid_keys.keys()
 # # audio["title"] = u"An example"
 # # audio.save()
