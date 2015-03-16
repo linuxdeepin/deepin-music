16b3cb81204ee27300bb24ce8005bb9f96facd15
djf <dingjiangfeng@linuxcdeepin.com>
2015-03-16 17:56:36 +0800

    fix

--- src/controllers/coverworker.py
+++ src/controllers/coverworker.py
@@ -15,12 +15,14 @@ from config.constants import CoverPath
 
 class CoverWorker(QObject):
 
-    coverUpdated = pyqtSignal('QString')
+    downloadSuccessed = pyqtSignal('QString')
 
     @registerContext
     def __init__(self, parent=None):
         super(CoverWorker, self).__init__(parent)
         self._covers = {}
+
+        # self.downloadSuccessed.connect(self.)
 
     @classmethod
     def md5(cls, string):
@@ -29,32 +31,19 @@ class CoverWorker(QObject):
         return md5Value.hexdigest()
 
     @classmethod
-    def getCoverPath(cls, url):
+    def getCoverPathByUrl(cls, url):
         coverID = cls.md5(url)
         filename = '%s' % coverID
         filepath = os.path.join(CoverPath, filename)
         return filepath
 
-    @pyqtSlot('QVariant', 'QString')
+    @pyqtSlot('QString')
     @dthread
-    def downloadCover(self, mediaContent):
-        cover = mediaContent.cover
-        url = mediaContent.url
-        if url in self._covers:
-            self.coverUpdated.emit(self._covers[url])
+    def downloadCover(self, coverUrl):
+        try:
+            r = requests.get(coverUrl)
+            with open(filepath, "wb") as f:
+                f.write(r.content)
+        except:
             return
-        else:
-            filepath = self.getCoverPath(url)
-            if os.path.exists(filepath):
-                self._covers.update({url: filepath})
-                self.coverUpdated.emit(filepath)
-                return
-            else:
-                try:
-                    r = requests.get(cover)
-                    with open(filepath, "wb") as f:
-                        f.write(r.content)
-                except:
-                    return
-                self._covers.update({url: filepath})
-                self.coverUpdated.emit(filepath)
+        self.downloadSuccessed.emit()
