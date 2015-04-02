d075567e98183b19598aff10806ab189639aef58
dragondjf <dragondjf@gmail.com>
2015-04-02 21:38:02 +0800

    fix

---
+++ src/controllers/net.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: omi
+# @Date:   2014-08-24 21:51:57
+# @Last Modified by:   omi
+# @Last Modified time: 2015-03-30 23:36:21
+
+
+'''
+网易云音乐 Api
+'''
+
+import re
+import json
+import requests
+# from bs4 import BeautifulSoup
+import logger
+import hashlib
+import random
+
+default_timeout = 10
+
+log = logger.getLogger(__name__)
+
+
+class NetEaseAPI(object):
+    def __init__(self):
+        self.header = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip,deflate,sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'music.163.com',
+            'Referer': 'http://music.163.com/search/',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
+        }
+        self.cookies = {
+            'appver': '1.5.2'
+        }
+        self.playlist_class_dict = {}
+
+    def httpRequest(self, method, action, query=None, urlencoded=None, callback=None, timeout=None):
+        connection = json.loads(self.rawHttpRequest(method, action, query, urlencoded, callback, timeout))
+        return connection
+
+    def rawHttpRequest(self, method, action, query=None, urlencoded=None, callback=None, timeout=None):
+        if (method == 'GET'):
+            url = action if (query == None) else (action + '?' + query)
+            connection = requests.get(url, headers=self.header, timeout=default_timeout)
+
+        elif (method == 'POST'):
+            connection = requests.post(
+                action,
+                data=query,
+                headers=self.header,
+                timeout=default_timeout
+            )
+
+        connection.encoding = "UTF-8"
+
+        print connection.url
+        return connection.text
+
+    # 搜索单曲(1)，歌手(100)，专辑(10)，歌单(1000)，用户(1002) *(type)*
+    def search(self, s, stype=1, offset=0, total='true', limit=60):
+        action = 'http://music.163.com/api/search/get/web'
+        data = {
+            's': s,
+            'type': stype,
+            'offset': offset,
+            'total': total,
+            'limit': 60
+        }
+        return self.httpRequest('POST', action, data)
+
+
+if __name__ == '__main__':
+    net = NetEaseAPI()
+    ret = net.search(u'边走边爱', stype=1)
+    with open('./result.json', 'wb') as f:
+        json.dump(ret, f, indent=4)
+    # print ret
