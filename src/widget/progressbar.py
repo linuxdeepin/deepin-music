1c9db2b16e886496ce6acf68cc52d915d7921e94
Hualet Wang <mr.asianwang@gmail.com>
2016-02-14 10:43:48 +0800

    improve copyright
    Change-Id: I7a5b7a2c64485ec621428ae3c104898f560292a4

--- src/widget/progressbar.py
+++ src/widget/progressbar.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2012 Deepin Technology Co., Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
 import gtk
 import gobject
 import cairo
--- src/widget/skin.py
+++ src/widget/skin.py
@@ -1,24 +1,12 @@
-from dtk.ui.theme import Theme, ui_theme
-from dtk.ui.skin_config import skin_config
-import os
-from deepin_utils.file import get_parent_dir
-from constant import FULL_DEFAULT_WIDTH, FULL_DEFAULT_HEIGHT, PROGRAM_VERSION
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Init skin config.
-skin_config.init_skin(
-    "sky",
-    os.path.join(get_parent_dir(__file__, 3), "skin"),
-    os.path.expanduser("~/.config/deepin-music-player/skin"),
-    os.path.expanduser("~/.config/deepin-music-player/skin_config.ini"),
-    "dmusic",
-    PROGRAM_VERSION
-    )
-
-# Create application theme.
-app_theme = Theme(
-    os.path.join(get_parent_dir(__file__, 3), "app_theme"),
-    os.path.expanduser("~/.config/deepin-music-player/theme")
-    )
+# Copyright (C) 2012 Deepin Technology Co., Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 
 # Set theme.
 skin_config.load_themes(ui_theme, app_theme)
