b2d5d3d7fac1954a5003ee78ca18ae33edb8a046
n913178 <zhenga.liu@archermind.com>
2019-11-08 14:35:42 +0800

    Change font bold

--- src/music-player/view/musiclistdatawidget.cpp
+++ src/music-player/view/musiclistdatawidget.cpp
@@ -664,7 +664,8 @@ MusicListDataWidget::MusicListDataWidget(QWidget *parent) :
     d->btPlayAll->setFixedHeight(30);
     d->btPlayAll->setFocusPolicy(Qt::NoFocus);
     auto btPlayAllFont = d->btPlayAll->font();
-    btPlayAllFont.setFamily("SourceHanSansSC-Medium");
+    btPlayAllFont.setFamily("SourceHanSansSC");
+    btPlayAllFont.setWeight(QFont::Medium);
     btPlayAllFont.setPixelSize(14);
     d->btPlayAll->setFont(btPlayAllFont);
 
