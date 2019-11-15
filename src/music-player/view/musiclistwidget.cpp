5158a6ca74fe3afb886cbb3b666c15a9fe2c74ce
yub.wang <yub.wang@deepin.io>
2019-11-15 14:25:11 +0800

    fix:Automatic skip song error
    fixed automatic skip song error

--- src/music-player/view/musiclistwidget.cpp
+++ src/music-player/view/musiclistwidget.cpp
@@ -285,8 +285,8 @@ void MusicListWidget::onSearchText(QString str)
 
 void MusicListWidget::onMusicPlayed(PlaylistPtr playlist, const MetaPtr meta)
 {
-    m_dataListView->onMusicPlayed(playlist, meta);
-    onCurrentChanged(playlist);
+//    m_dataListView->onMusicPlayed(playlist, meta);
+//    onCurrentChanged(playlist);
 }
 
 void MusicListWidget::focusOutEvent(QFocusEvent *event)
