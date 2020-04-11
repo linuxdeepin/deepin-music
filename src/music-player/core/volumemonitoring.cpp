7de5d826fc5522613f000accbfc3eb5b12be2224
dudecheng <dudecheng@uniontech.com>
2020-04-11 09:56:38 +0800

    Title:fix the problem that the music volume and the task bar volume accuracy difference of 1%
    Description:解决音乐音量和任务栏音量精度相差1%的问题
    
    RootCause:底层是double，上层取整时丢掉了小数位
    
    Solution:上层补1

--- src/music-player/core/volumemonitoring.cpp
+++ src/music-player/core/volumemonitoring.cpp
@@ -20,7 +20,7 @@
  */
 
 #include "volumemonitoring.h"
-
+#include <QtMath>
 #include <QTimer>
 #include <QDBusObjectPath>
 #include <QDBusInterface>
@@ -106,8 +106,8 @@ void VolumeMonitoring::timeoutSlot()
     //获取音量
     QVariant muteV = DBusUtils::redDBusProperty("com.deepin.daemon.Audio", sinkInputPath,
                                                 "com.deepin.daemon.Audio.SinkInput", "Mute");
-
-    int volume = volumeV.toDouble() * 100 + 0.1;
+    //取最小正整数
+    int volume = qFloor(volumeV.toDouble() * 100);
     bool mute = muteV.toBool();
 
     if (volume != d->oldVolume) {
