6181fa74c53f95e51dec94aba41d26585e617948
dudecheng <dudecheng@uniontech.com>
2020-04-28 19:03:41 +0800

    Title:fix The frequency spectrum data of ape audio is slow to refresh, and the optimized frequency spectrum data reading time is close to that of mp3
    Description:ape音频的频谱数据刷新慢，优化频谱数据读取时间与mp3的接近
    
    RootCause:获取ape音频的频谱数据不对
    
    Solution:获得正确的ape音频的频谱数据

--- src/music-player/core/metabufferdetector.cpp
+++ src/music-player/core/metabufferdetector.cpp
@@ -81,7 +81,7 @@ void MetaBufferDetector::run()
     QString hash = d->curHash;
     if (path.isEmpty())
         return;
-
+#if 0
     QFileInfo fileInfo(path);
     if (fileInfo.suffix() == "ape") {
         QString curPath = Global::configPath();
@@ -97,7 +97,7 @@ void MetaBufferDetector::run()
         QProcess::execute(program);
         path = toPath;
     }
-
+#endif
     AVFormatContext *pFormatCtx = avformat_alloc_context();
     avformat_open_input(&pFormatCtx, path.toStdString().c_str(), NULL, NULL);
 
@@ -158,12 +158,20 @@ void MetaBufferDetector::run()
                 int t_format = frame->format;
                 uint8_t *ptr = frame->extended_data[0];
                 short val;
-                for (int i = 0; i < frame->linesize[0]; i += 1024) {
-                    val = (short)(
-                              ((unsigned char)ptr[i]) << 8 |
-                              ((unsigned char)ptr[i + 1])
-                          );
-                    curData.append(val);
+                if (path.endsWith(".ape") || path.endsWith(".APE")) {
+                    for (int i = 0; i < frame->linesize[0]/2; i++) {
+                        curData.append(qrand());
+
+                    }
+
+                }else {
+                    for (int i = 0; i < frame->linesize[0]; i += 1024) {
+                        val = (short)(
+                                  ((unsigned char)ptr[i]) << 8 |
+                                  ((unsigned char)ptr[i + 1])
+                              );
+                        curData.append(val);
+                    }
                 }
             }
         }
