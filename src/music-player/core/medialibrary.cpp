f0604c64a82d4d6dac50bd6abc2124a508012925
tangpeng <tangpeng@uniontech.com>
2020-06-01 20:44:36 +0800

    Title: Change the policy to detect malformations
    Description: 改变策略，检测畸形文件
    
    RootCause:Null
    
    Solution:
    
    Reviewer:

--- src/music-player/core/medialibrary.cpp
+++ src/music-player/core/medialibrary.cpp
@@ -189,7 +189,6 @@ MetaPtr MediaLibraryPrivate::importMeta(const QString &filepath,
         return MetaPtr();
     }
 
-
     if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
         avformat_free_context(pFormatCtx);
         return MetaPtr();
@@ -197,8 +196,6 @@ MetaPtr MediaLibraryPrivate::importMeta(const QString &filepath,
 
     int audio_stream_index = -1;
     audio_stream_index = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
-
-
 
     if (audio_stream_index < 0) {
         avformat_close_input(&pFormatCtx);
@@ -218,85 +215,22 @@ MetaPtr MediaLibraryPrivate::importMeta(const QString &filepath,
     AVPacket *packet = av_packet_alloc();
     AVFrame *frame = av_frame_alloc();
 
-    if (pCodecCtx->channels > 20  || pCodecCtx->channels < 1 ) {
-
-        return MetaPtr();
-    }
-
-    bool InvalidDetection = true;
-
-    if (pCodecCtx->sample_rate == 8000 || pCodecCtx->sample_rate == 11025  || pCodecCtx->sample_rate == 16000 ||
-            pCodecCtx->sample_rate == 22050 || pCodecCtx->sample_rate == 44100 ||
-            pCodecCtx->sample_rate == 48000) {
-
-        InvalidDetection = true;
-    } else {
-        InvalidDetection = false;
-
-    }
-
-    if (pCodecCtx->channel_layout == 3 || pCodecCtx->channel_layout == 4) {
-
-        InvalidDetection = true;
-    } else {
-        InvalidDetection = false;
-    }
-
-    int layout = 0;
-    QString chaLayout = QString::number(pCodecCtx->channel_layout, 2);
-    QByteArray bytes = chaLayout.toUtf8();
-
-    for (int i = 0; i < bytes.size(); i++) {
-
-        if (bytes[i] == '1') {
-            layout++;
-        }
-    }
-
-    if (layout == pCodecCtx->channels) {
-
-        InvalidDetection = true;
-    }
-
-    if (pCodecCtx->channels == 2 && pCodecCtx->channel_layout == 0) {
-        qDebug() << "InvalidDetection is true " << filepath;
-        InvalidDetection = true;
-    }
-
-    if (!InvalidDetection) {
-
-        av_packet_unref(packet);
-        av_frame_free(&frame);
-        avcodec_close(pCodecCtx);
-        avformat_close_input(&pFormatCtx);
-        avformat_free_context(pFormatCtx);
-
-        return MetaPtr();
-    }
-
-    int erroCount = 0;
     int readCount = 0;
-    int packageErr = 0;
-    int packageCount = 0;
+    int sendCount = 0;
+    int receiveCount = 0;
 
     while ( av_read_frame(pFormatCtx, packet) >= 0 ) {
-
         if (packet->stream_index == audio_stream_index) {
-            int got_picture;
-            int ret = avcodec_decode_audio4( pCodecCtx, frame, &got_picture, packet);
-
-            packageCount++;
-
-            if ( ret < 20) {
-                packageErr++;
+            int ret;
+            ret = avcodec_send_packet(pCodecCtx, packet);
+            av_packet_unref(packet);
+            if (ret != 0) {
+                sendCount++;
             }
-            if ( got_picture <= 0 ) {
 
-                erroCount++;
-
-                if (erroCount > 5) {
-                    break;
-                }
+            ret = avcodec_receive_frame(pCodecCtx, frame);
+            if (ret != 0) {
+                receiveCount++;
             }
         }
         av_packet_unref(packet);
@@ -306,31 +240,39 @@ MetaPtr MediaLibraryPrivate::importMeta(const QString &filepath,
         }
     }
 
+    bool invalidFile = false;
+
+    if (sendCount != 0 || receiveCount != 0) {
+        if (sendCount == receiveCount) {
+            invalidFile = true;
+        }
+    }
+
+    if (readCount < 65) {
+        if (receiveCount > 2) {
+            invalidFile = true;
+        }
+    }
+
+    if (readCount == 33 && sendCount == 1 && receiveCount == 2) {
+        invalidFile = true;
+    }
+
+    if (invalidFile) {
+        av_packet_unref(packet);
+        av_frame_free(&frame);
+        avcodec_close(pCodecCtx);
+        avformat_close_input(&pFormatCtx);
+        avformat_free_context(pFormatCtx);
+
+        return MetaPtr();
+    }
+
     av_packet_unref(packet);
     av_frame_free(&frame);
     avcodec_close(pCodecCtx);
     avformat_close_input(&pFormatCtx);
     avformat_free_context(pFormatCtx);
-
-    if (filepath.endsWith(".ape") || filepath.endsWith(".APE")) {
-
-    } else {
-
-        if (erroCount > 5) {
-
-            return MetaPtr();
-        }
-
-        if (packageCount > 300) {
-
-        } else {
-
-            if (packageErr > 15) {
-
-                return MetaPtr();
-            }
-        }
-    }
 
     auto hash = DMusic::filepathHash(filepath);
     if (MediaLibrary::instance()->contains(hash)) {
