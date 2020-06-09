7dc74ba3e6c1dc2c75257769cd2327e22404f1ad
tangpeng <tangpeng@uniontech.com>
2020-06-09 18:45:24 +0800

    Title: FIX Delete playlist after switching playlist play, at this time 'all music' right-click menu status error.
    Description:
    bug : 20900 【音乐】【6.0.1.31-1】删除歌单后切换歌单播放，此时‘所有音乐’右键菜单状态错误。
    
    RootCause:Null
    
    Solution:
    
    Reviewer: 赵永铭

--- src/music-player/view/musiclistwidget.cpp
+++ src/music-player/view/musiclistwidget.cpp
@@ -184,20 +184,6 @@ MusicListWidget::MusicListWidget(QWidget *parent) : DWidget(parent)
         auto current = m_dataBaseListview->item(2);
         auto curPtr = m_dataBaseListview->playlistPtr(current);
 
-#if 0
-        if (curPtr != nullptr) {
-            if (meta != nullptr) {
-                curPtr->play(meta);
-                m_dataBaseListview->setCurPlaylist(current);
-                m_dataBaseListview->setCurrentItem(current);
-            }
-            m_customizeListview->clearSelected();
-            m_customizeListview->closeAllPersistentEditor();
-            m_dataListView->selectMusiclistChanged(curPtr);
-            curPtr->setSearchStr("");
-            Q_EMIT selectedPlaylistChange(curPtr);
-        }
-#endif
         Q_EMIT this->playall(curPtr);
     });
     connect(m_customizeListview, &MusicListView::customResort,
