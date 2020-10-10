123d75b8924be4b4cbe15d1aaf747b89a07d0041
tangpeng <tangpeng@uniontech.com>
2020-10-10 10:49:09 +0800

    fix: Music app Maximization, click hide tray, click show again is not maximized
    音乐应用最大化，点击托盘隐藏，再次点击显示不是最大化
    
    Log: Music app Maximization, click hide tray, click show again is not maximized
    Change-Id: Idf28eea9b1d3c8eea79d410cb6e0a30947ccdc6a
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/deepin-music/+/6947
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: wangyu <wangyu@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- music/music-player/view/mainframe.cpp
+++ music/music-player/view/mainframe.cpp
@@ -899,15 +899,20 @@ void MainFrame::postInitUI()
         if (QSystemTrayIcon::Trigger == reason) {
             if (isVisible()) {
                 if (isMinimized()) {
-                    this->setFocus();
-                    showNormal();
-                    activateWindow();
+                    if (isFullScreen()) {
+                        hide();
+                        showFullScreen();
+                    } else {
+                        this->titlebar()->setFocus();
+                        showNormal();
+                        activateWindow();
+                    }
                 } else {
                     showMinimized();
                     hide();
                 }
             } else {
-                this->setFocus();
+                this->titlebar()->setFocus();
                 showNormal();
                 activateWindow();
             }
