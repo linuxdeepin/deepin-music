faa96ad20a4617406cb05d6a436994ef5387a0b2
zouya <zouya@uniontech.com>
2020-11-12 20:55:12 +0800

    fix: unuse turbo
    不使用turbo
    
    Log: 不使用turbo
    Change-Id: I1dba81e495299ca0bce7e585148afd6cdfa47a9a

--- music/music-player/main.cpp
+++ music/music-player/main.cpp
@@ -111,11 +111,11 @@ bool checkOnly()
 int main(int argc, char *argv[])
 {
     setenv("PULSE_PROP_media.role", "music", 1);
-#if (DTK_VERSION < DTK_VERSION_CHECK(5, 4, 0, 0))
+//#if (DTK_VERSION < DTK_VERSION_CHECK(5, 4, 0, 0))
     DApplication *app = new DApplication(argc, argv);
-#else
-    DApplication *app = DApplication::globalApplication(argc, argv);
-#endif
+//#else
+//    DApplication *app = DApplication::globalApplication(argc, argv);
+//#endif
 
 #ifdef SNAP_APP
     DStandardPaths::setMode(DStandardPaths::Snap);
