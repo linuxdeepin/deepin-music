37948fd5c9a5912635a2f6399283a91011b6b5e2
Huang Jie <huangjie@uniontech.com>
2021-02-09 17:27:48 +0800

    fix: 移出gtest头文件依赖
    移出gtest的头文件依赖
    
    Log: 移出gtest头文件依赖
    Change-Id: I1c576215fca80459031591007d3e25f4f4b5c3f8

--- tests/collection-coverage.sh
+++ tests/collection-coverage.sh
@@ -1,6 +1,6 @@
 lcov --directory ./CMakeFiles/deepin-music-test.dir --zerocounters
 lcov --directory . --capture --output-file ./coverageResult/deepin-music-test_Coverage.info
 echo \ ===================\ do\ filter\ begin\ ====================\ 
-lcov --remove ./coverageResult/deepin-music-test_Coverage.info '*/deepin-music-test_autogen/*' '*/deepin-music-test_autogen/*' '*/usr/include/*' '*/tests/*' '*/googletest/*' -o ./coverageResult/deepin-music-test_Coverage.info
+lcov --remove ./coverageResult/deepin-music-test_Coverage.info '*/deepin-music-test_autogen/*' '*/deepin-music-test_autogen/*' '*/usr/include/*' '*/tests/*' '*/googletest/*' '*/gtest/*' -o ./coverageResult/deepin-music-test_Coverage.info
 echo \ ===================\ do\ filter\ end\ ====================\ 
 genhtml -o ./coverageResult/report ./coverageResult/deepin-music-test_Coverage.info
