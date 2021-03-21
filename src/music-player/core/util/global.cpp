67301dbf2371a89e4ab357278dda424c6fbdf8b6
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2021-03-21 12:22:45 -0930

    Add function to enable MPRIS artwork support
    Added function imagePushed(QFile &src) to save artwork ang generate the local file URL.

--- src/music-player/core/util/global.cpp
+++ src/music-player/core/util/global.cpp
@@ -24,12 +24,18 @@
 #include <QCoreApplication>
 #include <DStandardPaths>
 
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <iostream>
+
 DCORE_USE_NAMESPACE;
 QString appName;
+static QString imagePushFilePath = "/tmp/deepin-music/cover.jpg";
+
 QString Global::configPath()
 {
-    auto userConfigPath = DStandardPaths::standardLocations(QStandardPaths::AppConfigLocation).value(0);
-    return userConfigPath;
+    return DStandardPaths::standardLocations(QStandardPaths::AppConfigLocation).value(0);
 }
 
 QString Global::cacheDir()
@@ -48,3 +54,60 @@ QString Global::getAppName()
     return appName;
 }
 
+#define RECURSION_LIMIT 5
+
+static int recursion = 0;
+static bool firstTime = true;
+bool isPending = false; //Used in slotMediaMetaChanged(...) signal
+
+QString Global::imagePushed(QFile &src)
+{
+    QFileInfo info(imagePushFilePath);
+    if ((!info.exists() | !info.isWritable()) && firstTime == false && recursion<RECURSION_LIMIT) {
+        QString path = imagePushFilePath.remove(info.fileName());
+        path.chop(1);
+        path += "_/cover.jpg";
+        imagePushFilePath = path;
+        QDir().mkdir(path);
+        recursion++;
+        return imagePushed(src);
+    } else {
+        if (firstTime) {
+            firstTime = false;
+            //So create the dir first.
+            if (!QDir().mkdir("/tmp/deepin-music")) {
+                return imagePushed(src);
+            }
+        }
+        QFile f(imagePushFilePath);
+        if (f.open(QIODevice::OpenModeFlag::Truncate | QIODevice::OpenModeFlag::WriteOnly)) {
+            //SRC file checks, else abort OP
+            if (!src.isOpen()) {
+                if (!src.open(QIODevice::OpenModeFlag::ReadOnly)) {
+                    return "";
+                }
+            }
+            if (src.isReadable()) {
+                f.write(src.readAll(), src.size());
+                f.flush();
+                f.close();
+                recursion = 0;
+                std::cout << "File saved." << std::endl;
+                return imagePushFilePath;
+            }
+            return "";
+        } else if (recursion<RECURSION_LIMIT) {//We can remove it as at a time it'll for sure find a good path to use
+            //Maybe an error we haven't caught.
+            QString path = imagePushFilePath.remove(info.fileName());
+            path.chop(1);
+            path += "_/cover.jpg";
+            imagePushFilePath = path;
+            QDir().mkdir(path);
+            recursion++;
+            return imagePushed(src);
+        } else {
+            recursion = 0;
+            return "";
+        }
+    }
+}
