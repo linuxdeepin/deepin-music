28b325ef28324644603f4d1483c25a59ffa8bc43
Wang Yu <wangyu@uniontech.com>
2021-11-01 16:47:36 +0800

    fix: 修改了搜索结果的专辑页面修改排序不生效
    修改了搜索结果的专辑页面修改排序不生效
    
    Log: 修改了搜索结果的专辑页面修改排序不生效
    Bug: https://pms.uniontech.com/zentao/bug-view-100678.html
    Change-Id: I4ac4c977b71c86bd89b4c1ad5298c1713fd157b6

--- src/music-player/listView/albumList/albumlistview.cpp
+++ src/music-player/listView/albumList/albumlistview.cpp
@@ -720,9 +720,9 @@ void AlbumListView::setSortType(DataBaseService::ListSortType sortType)
 void AlbumListView::setDataBySortType(QList<AlbumInfo> &albumInfos, DataBaseService::ListSortType sortType)
 {
     // 不加入到搜索结果中
-    if (m_hash == "albumResult" || m_hash == "artistResult") {
-        return;
-    }
+//    if (m_hash == "albumResult" || m_hash == "artistResult") {
+//        return;
+//    }
     // 排序
     sortList(albumInfos, sortType);
 
