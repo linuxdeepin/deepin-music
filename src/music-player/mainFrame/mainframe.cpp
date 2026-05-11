4efd4c8404e8b90938170230220fa7d02c2491c2
Resurgamz <108165585+Resurgamz@users.noreply.github.com>
2026-05-11 18:02:16 +0800

    fix: The maximize button in the top-right corner shows hover state on mouseover in the music app. (#671)
    This reverts commit 79274ab.
    
    Log: 修复音乐应用最大化按钮hover菜单无法显示的问题
    Bug: https://pms.uniontech.com/bug-view-355491.html
    Influence: 当鼠标移动到右上角最大化时显示hover状态，可选择将窗口拼贴到屏幕左侧、将窗口拼贴到屏幕右侧、还原
    
    Co-authored-by: zhangjiarui <zhangjiarui@uniontech.com>

--- src/music-player/mainFrame/mainframe.cpp
+++ src/music-player/mainFrame/mainframe.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 ~ 2021 Uniontech Software Technology Co., Ltd.
+// Copyright (C) 2020 ~ 2026 Uniontech Software Technology Co., Ltd.
 // SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
